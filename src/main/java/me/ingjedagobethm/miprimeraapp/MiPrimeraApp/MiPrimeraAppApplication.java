650735d9ab495bc073ad493b5fdc67c9c276ca06
IngJEDagobethM <x.game19@gmail.com>
2022-05-16 21:32:04 -0500

    first commit

---
+++ src/main/java/me/ingjedagobethm/miprimeraapp/MiPrimeraApp/MiPrimeraAppApplication.java
@@ -0,0 +1,13 @@
+package me.ingjedagobethm.miprimeraapp.MiPrimeraApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiPrimeraAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiPrimeraAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/ingjedagobethm/miprimeraapp/MiPrimeraApp/MiPrimeraAppApplicationTests.java
@@ -0,0 +1,13 @@
+package me.ingjedagobethm.miprimeraapp.MiPrimeraApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiPrimeraAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
